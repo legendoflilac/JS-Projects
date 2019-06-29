5962e786cc875094f6b42175d4dc5d5fd93f816a
R Irwin <43350178+legendoflilac@users.noreply.github.com>
2019-06-29 12:39:32 -0500

    File setup

---
+++ palindrome-checker.js
@@ -0,0 +1,16 @@
+/*
+Palindrome Checker
+https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/palindrome-checker
+Return true or false
+Must remove all non-alphanumeric characters
+Must accept strings with various formats: "racecar" "RaceCar" and "race CAR"
+*/
+
+function palindrome(str) {
+    // Good luck!
+    return true;
+  }
+  
+  
+  
+  palindrome("eye");
