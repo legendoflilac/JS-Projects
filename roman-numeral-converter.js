2ebe0aeec0ac4e2bbfb4aee14b57f4ce5aaee1a0
R Irwin <43350178+legendoflilac@users.noreply.github.com>
2019-07-19 14:38:18 -0500

    Fixed loop issue

--- roman-numeral-converter.js
+++ roman-numeral-converter.js
@@ -5,41 +5,22 @@ ex. 2 returns "II", 9 returns "IX", 3999 returns "MMMCMXCIX"
 creating arrays/objects, one with roman numerals and one with
 decimal equivalent
 no more than three of the same consecutive Roman numerals together
-
-1,5,10,50,100,500,1000
-I,V,X,L,C,D,M
-
-add in 4s and 9s
-1,4,5,9,10,40,50,90,100,400,500,900,1000
-I,IV,V,IX,X,XL,L,XC,C,CD,D,CM,M
-
-ex. 36
-find largest that goes into it, in this case 10
-XXX
-
-unless going right to left would be easier?
-36
-XXXVI
 */
 
 function convertToRoman(num) {
-    decimalArray = [1,4,5,9,10,40,50,90,100,400,500,900,1000];
-    romanArray = ['I','IV','V','IX','X','XL','L','XC','C','CD','D','CM','M'];
-    //how many thous we got?
-    //go into array subtract 1000, add M
-    //now have 0 and 3 Ms
-    //am I zero? yes, 3 ms
-    //600
-    //thousands nope
-    //900 nope
-    //500 away
-    //L
-    //X
-    while (num != 0) {
-        if 
-    }
-
-    return num;
-   }
+    const decimalArray = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
+    const romanArray = ['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I'];
+    let strArray = [];
+    let counter = 0;
+    while (num > 0) {
+        if (num >= decimalArray[counter]) {
+            num = num-decimalArray[counter];
+            strArray.push(romanArray[counter]);
+        }
+        else {
+            counter++;
+        }
+    } return strArray.join('');
+}
    
-   convertToRoman(36);
+console.log(convertToRoman(39));
