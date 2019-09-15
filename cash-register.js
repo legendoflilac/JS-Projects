f5ee20687c393a623db3fc7c6c1782bff72e67f6
R Irwin <43350178+legendoflilac@users.noreply.github.com>
2019-09-15 16:33:10 -0500

    added comments

--- cash-register.js
+++ cash-register.js
@@ -15,50 +15,50 @@ Cash register
       var totalInDrawer = 0;
       var values = [["PENNY", 0.01], ["NICKEL", 0.05], ["DIME", 0.1], ["QUARTER", 0.25], ["ONE", 1], ["FIVE", 5], ["TEN", 10], ["TWENTY", 20], ["ONE HUNDRED", 100]];
       var changeArray = [];
-
+      //find total in drawer
       for (var totalIndex = 0; totalIndex<cid.length;totalIndex++) {
           totalInDrawer = totalInDrawer + cid[totalIndex][1]
       }
-      totalInDrawer = totalInDrawer.toFixed(2) * 1
+      totalInDrawer = totalInDrawer.toFixed(2) * 1 //toFixed returns string, multiply by 1 to convert back to Number
       console.log('Total in Drawer is: ', totalInDrawer)
-      if (totalInDrawer == change) {
+      if (totalInDrawer == change) { //if we have the same amount in the drawer as need for change, then the program finishes
           return {status: "CLOSED", change: cid} 
       }
-      else if (totalInDrawer < change) {
+      else if (totalInDrawer < change) { //if we can't make change due to not having enough money, the program finishes
           return {status: "INSUFFICIENT_FUNDS", change: []}
       }
-      else {
-        var moneyValue = 8;
+      else { //otherwise, find the money we need to make change
+        var moneyValue = 8; //this index works back-to-front on the values array
         while (change > 0) {
             if (moneyValue < 0) {
-                return {status: "INSUFFICIENT_FUNDS", change: []};
+                return {status: "INSUFFICIENT_FUNDS", change: []}; //if we reach here, we've exhausted all other options on previous loops
             }
-            else if (change - values[moneyValue][1] < 0) {
+            else if (change - values[moneyValue][1] < 0) { //if change minus the denomination is less than 0, find a new denomination
                 moneyValue = moneyValue - 1;
             }
-            else if (change - values[moneyValue][1] >= 0) { 
-                if (cid[moneyValue][1] > 0) {
-                    change = (change - values[moneyValue][1]).toFixed(2) * 1
-                    changeArray.push(values[moneyValue])
+            else if (change - values[moneyValue][1] >= 0) {
+                if (cid[moneyValue][1] > 0) { //if we have the denomination in the drawer
+                    change = (change - values[moneyValue][1]).toFixed(2) * 1 //do the math plus converting back to Number
+                    changeArray.push(values[moneyValue]) //push this value to the new Array
                     console.log('Pushing: ', values[moneyValue])
-                    if (changeArray.length >= 2) {
+                    if (changeArray.length >= 2) { //if we push two twenties, we need to have the result say ["TWENTY", 40]. Below is my solution for that
                         for (var i=changeArray.length-1; i>0; i--) {
-                            if (changeArray[i][0]==changeArray[i-1][0]) {
-                                changeArray[i-1] = [changeArray[i-1][0], changeArray[i-1][1]+changeArray[i][1]]
-                                changeArray.pop(i)
+                            if (changeArray[i][0]==changeArray[i-1][0]) { //if "TWENTY" == "TWENTY")
+                                changeArray[i-1] = [changeArray[i-1][0], changeArray[i-1][1]+changeArray[i][1]] //then change the first "TWENTY" array to be ["TWENTY", 40]
+                                changeArray.pop(i) //take off the last/most recent item added--it's already been counted
                             }
                         }
                       }
-                      cid[moneyValue][1] = cid[moneyValue][1]-values[moneyValue][1]
+                      cid[moneyValue][1] = cid[moneyValue][1]-values[moneyValue][1] //subtract the money from the drawer
                 }
                 else {
-                    moneyValue = moneyValue - 1;
+                    moneyValue = moneyValue - 1; //otherwise move a denomination
                 }
             }
 
         }
         }
-        return {status: 'OPEN', change: changeArray};
+        return {status: 'OPEN', change: changeArray}; //we can give change and have money in the drawer--return the change array
       }
   
   checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
