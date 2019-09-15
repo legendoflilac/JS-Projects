42fc7803ba0bc421a428d77b6dd81eeb5b2678d2
R Irwin <43350178+legendoflilac@users.noreply.github.com>
2019-09-15 16:21:39 -0500

    passes tests--is a mess

--- cash-register.js
+++ cash-register.js
@@ -10,76 +10,55 @@ Cash register
 
 
   function checkCashRegister(price, cash, cid) {
-      var change = price - cash //change due to customer
+      var change = cash - price //change due to customer
+      console.log('Change Due is: ', change)
       var totalInDrawer = 0;
       var values = [["PENNY", 0.01], ["NICKEL", 0.05], ["DIME", 0.1], ["QUARTER", 0.25], ["ONE", 1], ["FIVE", 5], ["TEN", 10], ["TWENTY", 20], ["ONE HUNDRED", 100]];
       var changeArray = [];
-      //calculate current amount of cash in box? from 100 to penny, then from 20 to penny. then if this amount is greater than change, we know we can start the change loop.
-      //if amount from 100-penny is equal to change due, then do the closed ending.
+
       for (var totalIndex = 0; totalIndex<cid.length;totalIndex++) {
           totalInDrawer = totalInDrawer + cid[totalIndex][1]
       }
+      totalInDrawer = totalInDrawer.toFixed(2) * 1
+      console.log('Total in Drawer is: ', totalInDrawer)
       if (totalInDrawer == change) {
-          return {status: "CLOSED", cid} //will need to be sorted high to low
+          return {status: "CLOSED", change: cid} 
       }
       else if (totalInDrawer < change) {
-          return {status: "INSUFFICIENT FUNDS", change: []}
+          return {status: "INSUFFICIENT_FUNDS", change: []}
       }
       else {
-          //status is open for now, move on to see if we can give exact change
+        var moneyValue = 8;
+        while (change > 0) {
+            if (moneyValue < 0) {
+                return {status: "INSUFFICIENT_FUNDS", change: []};
+            }
+            else if (change - values[moneyValue][1] < 0) {
+                moneyValue = moneyValue - 1;
+            }
+            else if (change - values[moneyValue][1] >= 0) { 
+                if (cid[moneyValue][1] > 0) {
+                    change = (change - values[moneyValue][1]).toFixed(2) * 1
+                    changeArray.push(values[moneyValue])
+                    console.log('Pushing: ', values[moneyValue])
+                    if (changeArray.length >= 2) {
+                        for (var i=changeArray.length-1; i>0; i--) {
+                            if (changeArray[i][0]==changeArray[i-1][0]) {
+                                changeArray[i-1] = [changeArray[i-1][0], changeArray[i-1][1]+changeArray[i][1]]
+                                changeArray.pop(i)
+                            }
+                        }
+                      }
+                      cid[moneyValue][1] = cid[moneyValue][1]-values[moneyValue][1]
+                }
+                else {
+                    moneyValue = moneyValue - 1;
+                }
+            }
+
+        }
+        }
+        return {status: 'OPEN', change: changeArray};
       }
-
-      //convert change to numbers of coins/bills and then back? could be easier
-      for (var index = 0; index<cid.length; index++) {
-          cid[index][1] = cid[index][1]/values[index][1] 
-          console.log(cid);
-      }
-
-
-
-      while (change >= 0) {
-          var moneyValue = 8;
-          if (change - values[moneyValue][1] < 0) {
-              moneyValue - 1;
-          }
-          else if (change - values[moneyValue][1] == 0) { //if change (say .25) minus values[4][1] == 0
-              if (cid[moneyValue][1] > 0) {
-                  change = change - values[moneyValue][1]
-                  changeArray.push(values[moneyValue])
-              }
-              else {
-                  moneyValue - 1
-              }
-          }
-          else if (change - values[moneyValue][1] > 0) {
-
-          }
-      }
-
-      for (var moneyValue; moneyValue<cid.length; moneyValue++) {
-          //iterate backwards over the value array to see what largest denomination we can put towards change ? modulo?
-          if (change - values[moneyValue] < 0)
-          //if subtracting value doesn't make it negative, how many of that denomination can we hypothetically put towards the change?
-          //then if it doesn't make it negative, we can ask cid if we have that denomination.
-          //if we do, can we do this for the same number of times we hypothetically did?
-          //if we do, then subtract the amount from the cid
-          //if we don't, then look to the next lowest denomination. do we have enough to add up to one of the larger denomination?
-          //if we do, then add that to our change array and continue until change equals zero.
-          //if we don't then we can't give change and get the insufficient funds ending.
-          //if we get everything to work then we get the normal ending.
-      }
-      return change;
-  }
-  
-  // Example cash-in-drawer array:
-  // [["PENNY", 1.01],
-  // ["NICKEL", 2.05],
-  // ["DIME", 3.1],
-  // ["QUARTER", 4.25],
-  // ["ONE", 90],
-  // ["FIVE", 55],
-  // ["TEN", 20],
-  // ["TWENTY", 60],
-  // ["ONE HUNDRED", 100]]
   
   checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
